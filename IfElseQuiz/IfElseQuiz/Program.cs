0c460bae0a359691b5ccfc1882f4f38b6b86f7e8
Hannah Andrews <handrews02@users.noreply.github.com>
2018-11-19 12:18:26 -0600

    update text

---
+++ IfElseQuiz/IfElseQuiz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringUserGuess;
+            int userGuess;
+            Console.WriteLine("Pick a number 1, 2, or 3");
+            stringUserGuess = Console.ReadLine();
+            userGuess = Convert.ToInt32(stringUserGuess);
+
+
+            if (userGuess == 1)
+            {
+                Console.WriteLine("You are WRONG!");
+
+            }
+            else if (userGuess == 2)
+            {
+                Console.WriteLine("Yay you are correct!");
+            }
+            else if(userGuess == 3)
+            {
+                Console.WriteLine("You are WRONG!");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+
+        }
+    }
+}
