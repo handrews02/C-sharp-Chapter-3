f5b6af0e47712133ff0286a46eec2f361dce4640
Hannah Andrews <handrews02@users.noreply.github.com>
2018-11-19 15:22:09 -0600

    update txt

---
+++ TestsInteractiveGUI/TestsInteractiveGUI/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace TestsInteractiveGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTest1 = new System.Windows.Forms.Label();
+            this.lblTest2 = new System.Windows.Forms.Label();
+            this.lblTest3 = new System.Windows.Forms.Label();
+            this.lblTest4 = new System.Windows.Forms.Label();
+            this.lblTest5 = new System.Windows.Forms.Label();
+            this.txtTest1 = new System.Windows.Forms.TextBox();
+            this.txttest5 = new System.Windows.Forms.TextBox();
+            this.txtTest4 = new System.Windows.Forms.TextBox();
+            this.txtTest3 = new System.Windows.Forms.TextBox();
+            this.txtTest2 = new System.Windows.Forms.TextBox();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTest1
+            // 
+            this.lblTest1.AutoSize = true;
+            this.lblTest1.Location = new System.Drawing.Point(110, 62);
+            this.lblTest1.Name = "lblTest1";
+            this.lblTest1.Size = new System.Drawing.Size(128, 13);
+            this.lblTest1.TabIndex = 0;
+            this.lblTest1.Text = "Enter your score for test 1";
+            // 
+            // lblTest2
+            // 
+            this.lblTest2.AutoSize = true;
+            this.lblTest2.Location = new System.Drawing.Point(110, 102);
+            this.lblTest2.Name = "lblTest2";
+            this.lblTest2.Size = new System.Drawing.Size(128, 13);
+            this.lblTest2.TabIndex = 1;
+            this.lblTest2.Text = "Enter your score for test 2";
+            // 
+            // lblTest3
+            // 
+            this.lblTest3.AutoSize = true;
+            this.lblTest3.Location = new System.Drawing.Point(110, 142);
+            this.lblTest3.Name = "lblTest3";
+            this.lblTest3.Size = new System.Drawing.Size(128, 13);
+            this.lblTest3.TabIndex = 2;
+            this.lblTest3.Text = "Enter your score for test 3";
+            // 
+            // lblTest4
+            // 
+            this.lblTest4.AutoSize = true;
+            this.lblTest4.Location = new System.Drawing.Point(110, 185);
+            this.lblTest4.Name = "lblTest4";
+            this.lblTest4.Size = new System.Drawing.Size(128, 13);
+            this.lblTest4.TabIndex = 3;
+            this.lblTest4.Text = "Enter your score for test 4";
+            // 
+            // lblTest5
+            // 
+            this.lblTest5.AutoSize = true;
+            this.lblTest5.Location = new System.Drawing.Point(110, 226);
+            this.lblTest5.Name = "lblTest5";
+            this.lblTest5.Size = new System.Drawing.Size(128, 13);
+            this.lblTest5.TabIndex = 4;
+            this.lblTest5.Text = "Enter your score for test 5";
+            // 
+            // txtTest1
+            // 
+            this.txtTest1.Location = new System.Drawing.Point(305, 59);
+            this.txtTest1.Name = "txtTest1";
+            this.txtTest1.Size = new System.Drawing.Size(100, 20);
+            this.txtTest1.TabIndex = 5;
+            // 
+            // txttest5
+            // 
+            this.txttest5.Location = new System.Drawing.Point(305, 223);
+            this.txttest5.Name = "txttest5";
+            this.txttest5.Size = new System.Drawing.Size(100, 20);
+            this.txttest5.TabIndex = 6;
+            // 
+            // txtTest4
+            // 
+            this.txtTest4.Location = new System.Drawing.Point(305, 182);
+            this.txtTest4.Name = "txtTest4";
+            this.txtTest4.Size = new System.Drawing.Size(100, 20);
+            this.txtTest4.TabIndex = 7;
+            // 
+            // txtTest3
+            // 
+            this.txtTest3.Location = new System.Drawing.Point(305, 139);
+            this.txtTest3.Name = "txtTest3";
+            this.txtTest3.Size = new System.Drawing.Size(100, 20);
+            this.txtTest3.TabIndex = 8;
+            // 
+            // txtTest2
+            // 
+            this.txtTest2.Location = new System.Drawing.Point(305, 99);
+            this.txtTest2.Name = "txtTest2";
+            this.txtTest2.Size = new System.Drawing.Size(100, 20);
+            this.txtTest2.TabIndex = 9;
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(113, 297);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(115, 55);
+            this.btnAverage.TabIndex = 10;
+            this.btnAverage.Text = "Click here to get your average!";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAverage.Location = new System.Drawing.Point(291, 316);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(114, 23);
+            this.lblAverage.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.txtTest2);
+            this.Controls.Add(this.txtTest3);
+            this.Controls.Add(this.txtTest4);
+            this.Controls.Add(this.txttest5);
+            this.Controls.Add(this.txtTest1);
+            this.Controls.Add(this.lblTest5);
+            this.Controls.Add(this.lblTest4);
+            this.Controls.Add(this.lblTest3);
+            this.Controls.Add(this.lblTest2);
+            this.Controls.Add(this.lblTest1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTest1;
+        private System.Windows.Forms.Label lblTest2;
+        private System.Windows.Forms.Label lblTest3;
+        private System.Windows.Forms.Label lblTest4;
+        private System.Windows.Forms.Label lblTest5;
+        private System.Windows.Forms.TextBox txtTest1;
+        private System.Windows.Forms.TextBox txttest5;
+        private System.Windows.Forms.TextBox txtTest4;
+        private System.Windows.Forms.TextBox txtTest3;
+        private System.Windows.Forms.TextBox txtTest2;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
+
